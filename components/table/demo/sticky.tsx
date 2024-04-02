58312e235e55bff4561b516b387ea34b43fab973
Sanjana0399 <165284450+Sanjana0399@users.noreply.github.com>
2024-04-02 16:06:13 +0530

    Update sticky.tsx
    Signed-off-by: Sanjana0399 <165284450+Sanjana0399@users.noreply.github.com>

--- components/table/demo/sticky.tsx
+++ components/table/demo/sticky.tsx
@@ -30,61 +30,11 @@ const columns: TableColumnsType<DataType> = [
     key: '1',
     width: 150,
   },
-  {
-    title: 'Column 2',
-    dataIndex: 'address',
-    key: '2',
-    width: 150,
-  },
-  {
-    title: 'Column 3',
-    dataIndex: 'address',
-    key: '3',
-    width: 150,
-  },
-  {
-    title: 'Column 4',
-    dataIndex: 'address',
-    key: '4',
-    width: 150,
-  },
-  {
-    title: 'Column 5',
-    dataIndex: 'address',
-    key: '5',
-    width: 150,
-  },
-  {
-    title: 'Column 6',
-    dataIndex: 'address',
-    key: '6',
-    width: 150,
-  },
-  {
-    title: 'Column 7',
-    dataIndex: 'address',
-    key: '7',
-    width: 150,
-  },
-  { title: 'Column 8', dataIndex: 'address', key: '8' },
-  {
-    title: 'Action',
-    key: 'operation',
-    fixed: 'right',
-    width: 100,
-    render: () => <a>action</a>,
-  },
+
+  
 ];
 
 const data: DataType[] = [];
-for (let i = 0; i < 100; i++) {
-  data.push({
-    key: i,
-    name: `Edward ${i}`,
-    age: 32,
-    address: `London Park no. ${i}`,
-  });
-}
 
 const App: React.FC = () => {
   const [fixedTop, setFixedTop] = useState(false);
@@ -94,26 +44,6 @@ const App: React.FC = () => {
       columns={columns}
       dataSource={data}
       scroll={{ x: 1500 }}
-      summary={() => (
-        <Table.Summary fixed={fixedTop ? 'top' : 'bottom'}>
-          <Table.Summary.Row>
-            <Table.Summary.Cell index={0} colSpan={2}>
-              <Switch
-                checkedChildren="Fixed Top"
-                unCheckedChildren="Fixed Top"
-                checked={fixedTop}
-                onChange={() => {
-                  setFixedTop(!fixedTop);
-                }}
-              />
-            </Table.Summary.Cell>
-            <Table.Summary.Cell index={2} colSpan={8}>
-              Scroll Context
-            </Table.Summary.Cell>
-            <Table.Summary.Cell index={10}>Fix Right</Table.Summary.Cell>
-          </Table.Summary.Row>
-        </Table.Summary>
-      )}
       // antd site header height
       sticky={{ offsetHeader: 64 }}
     />
